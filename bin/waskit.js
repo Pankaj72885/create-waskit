9e90166d92ae285b44371f8f5d5f11b2bce3cb49
Pankaj Bepari <beparipankaj@gmail.com>
2025-06-06 18:34:47 +0600

    fix bug

--- bin/waskit.js
+++ bin/waskit.js
@@ -345,7 +345,7 @@ async function main() {
   program
     .name("create-waskit")
     .description("A modern web project generator for the discerning developer.")
-    .version("0.0.20");
+    .version("0.0.21");
 
   program
     .argument("[project-directory]", "The directory for the new project")
